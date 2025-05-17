f5c0952fa15acaa64d93d7ea416f7c3e3eef51e4
Micah Joseph <micj88799@gmail.com>
2025-05-18 00:32:59 +0530

    Initial commit for backend

---
+++ jobHuntProject/src/main/java/com/JobHunt/jobHuntProject/JobHuntProjectApplication.java
@@ -0,0 +1,13 @@
+package com.JobHunt.jobHuntProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobHuntProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobHuntProjectApplication.class, args);
+	}
+
+}
---
+++ jobHuntProject/src/test/java/com/JobHunt/jobHuntProject/JobHuntProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JobHunt.jobHuntProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobHuntProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
